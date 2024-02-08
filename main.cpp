bb752c32dc9a12f09de61614ab4e966cfe25ce82
alwinsDen <jsmadesimple@gmail.com>
2024-02-08 20:45:18 +0530

    Test2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include <gtk/gtk.h>
 
 static void activate(GtkApplication *app, gpointer user_data) {
-
+    GtkWidget *window;
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc,
